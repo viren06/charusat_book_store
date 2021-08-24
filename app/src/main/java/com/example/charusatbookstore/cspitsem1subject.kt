80f1b6362a42f3252f2dbc26dad34378644d8704
mihir9111 <patelmihir9111@gmail.com>
2021-08-24 13:57:27 +0530

    by mihir

---
+++ app/src/main/java/com/example/charusatbookstore/cspitsem1subject.kt
@@ -0,0 +1,11 @@
+package com.example.charusatbookstore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class cspitsem1subject : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cspitsem1subject)
+    }
+}
--- app/src/main/java/com/example/charusatbookstore/sem.kt
+++ app/src/main/java/com/example/charusatbookstore/sem.kt
@@ -1,11 +1,20 @@
 package com.example.charusatbookstore
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class sem : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sem)
+
+        val iv1=findViewById<ImageView>(R.id.sem1)
+        iv1.setOnClickListener {
+            val Intent = Intent(this,cspitsem1subject::class.java)
+            startActivity(Intent)
+        }
     }
+
 }
