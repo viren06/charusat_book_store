e45b7dd513b47a926f193f9549920303a1827b26
Ridham <19dce112@charusat.edu.in>
2021-08-27 08:38:54 +0530

    Ridham

---
+++ app/src/main/java/com/example/charusatbookstore/Depstar_CE_Sem1_Subject.kt
@@ -0,0 +1,11 @@
+package com.example.charusatbookstore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Depstar_CE_Sem1_Subject : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_depstar_ce_sem1_subject)
+    }
+}
---
+++ app/src/main/java/com/example/charusatbookstore/Depstar_CE_Sem4_Subject.kt
@@ -0,0 +1,11 @@
+package com.example.charusatbookstore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Depstar_CE_Sem4_Subject : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_depstar_ce_sem4_subject)
+    }
+}
