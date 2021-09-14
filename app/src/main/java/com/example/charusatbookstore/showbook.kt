af271e59185c4813d756ca37d0b70275974f0b6d
mihir9111 <patelmihir9111@gmail.com>
2021-09-14 11:50:56 +0530

    upload book click to show price and subject and description by mihir

--- app/src/main/java/com/example/charusatbookstore/showbook.kt
+++ app/src/main/java/com/example/charusatbookstore/showbook.kt
@@ -1,7 +1,9 @@
 package com.example.charusatbookstore
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 
 class showbook : AppCompatActivity() {
@@ -9,6 +11,17 @@ class showbook : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_showbook)
 
+        //get data from intent
+        var intent = intent
+        val subject = intent.getStringExtra("Subject")
+        val price = intent.getStringExtra("Price")
+        val description = intent.getStringExtra("Description")
+
+        //textview
+        val resulttv = findViewById<TextView>(R.id.showdataid)
+
+        //settext
+        resulttv.text = "Subject :-"+subject+"\nPrice of Book :-"+price+"\nDescription of Book :-"+description
         Toast.makeText(applicationContext,"Upload successfully !!!",Toast.LENGTH_LONG).show()
     }
 }
--- app/src/main/java/com/example/charusatbookstore/upload.kt
+++ app/src/main/java/com/example/charusatbookstore/upload.kt
@@ -4,27 +4,39 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Spinner
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import android.widget.Toast.makeText as makeText1
+
+
 
 class upload : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_upload)
         //val college = findViewById<Spinner>(R.id.spinner)
-        val uploadbtn = findViewById<Button>(R.id.button20)
-        //val tvshow = findViewById<TextView>(R.id.textView8)
+        //val department = findViewById<Spinner>(R.id.spinner2)
+        //val semester = findViewById<Spinner>(R.id.spinner3)
+        val subjecttext = findViewById<EditText>(R.id.subjectetid)
+        val pricetext = findViewById<EditText>(R.id.priceetid)
+        val descriptiontext = findViewById<EditText>(R.id.descriptionetid)
+        val uploadbtn = findViewById<Button>(R.id.uploadbuttonid)
         uploadbtn.setOnClickListener {
-            val Intent = Intent(this,showbook::class.java)
-            startActivity(Intent)
-            //tvshow.getInputExtras()
+
+           // var clgvar = college.Spinner.toString()
+            //var devar = subjecttext.text.toString()
+            //var semvar = subjecttext.text.toString()
+            var subvar = subjecttext.text.toString()
+            var pvar = pricetext.text.toString()
+            var dpvar = descriptiontext.text.toString()
+            val intent = Intent(this,showbook::class.java)
+            intent.putExtra("Subject",subvar)
+            intent.putExtra("Price",pvar)
+            intent.putExtra("Description",dpvar)
+            startActivity(intent)
 
         }
     }
 }
 
-//private fun TextView.getInputExtras() {
-//}
+
+
