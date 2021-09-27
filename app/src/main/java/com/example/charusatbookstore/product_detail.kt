237e815c64bd3643ff6f5b211ffdd2973919e4b5
Ridham <19dce112@charusat.edu.in>
2021-09-27 18:36:25 +0530

    ridham

---
+++ app/src/main/java/com/example/charusatbookstore/product_detail.kt
@@ -0,0 +1,11 @@
+package com.example.charusatbookstore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class product_detail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_detail)
+    }
+}
