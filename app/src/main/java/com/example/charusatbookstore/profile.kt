1c0027cc89d27a35190f40f200a60c18d4a61409
mihir9111 <patelmihir9111@gmail.com>
2021-09-30 10:46:02 +0530

    show book which is upload by user for user by mihir

--- app/src/main/java/com/example/charusatbookstore/profile.kt
+++ app/src/main/java/com/example/charusatbookstore/profile.kt
@@ -1,15 +1,11 @@
 package com.example.charusatbookstore
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
-import android.widget.Toast
 
-class showbook : AppCompatActivity() {
+class profile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_showbook)
-
+        setContentView(R.layout.activity_profile)
     }
 }
