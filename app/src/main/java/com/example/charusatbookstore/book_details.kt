991e7dfe84cc8bfbaa1d7e78cd3dd9241fbf4085
Ridham <19dce112@charusat.edu.in>
2021-10-20 16:18:50 +0530

    Mail and sms sent successfull and option menu set in department page

--- app/src/main/java/com/example/charusatbookstore/book_details.kt
+++ app/src/main/java/com/example/charusatbookstore/book_details.kt
@@ -36,6 +36,7 @@ class book_details : AppCompatActivity() {
         var number=findViewById<TextView>(R.id.textView23)
         var bookimage=findViewById<ImageView>(R.id.imageView)
         var btnmail=findViewById<Button>(R.id.button28)
+        var btnsms=findViewById<Button>(R.id.btnsms)
         var bid=intent.getStringExtra("bid")
         var bookname=intent.getStringExtra("bname")
         var price=intent.getStringExtra("price")
@@ -47,6 +48,7 @@ class book_details : AppCompatActivity() {
         var user=mauth!!.currentUser
         if(flag=="1"){
             btnmail.visibility= View.GONE
+            btnsms.visibility= View.GONE
         }
 
         val myref=database.getReference("Book")
@@ -73,6 +75,27 @@ class book_details : AppCompatActivity() {
                 TODO("Not yet implemented")
             }
         })
+
+        btnmail.setOnClickListener(View.OnClickListener { view ->
+            // Do some work here
+            val to = "${email.text}"
+            val subject = "I want To Buy ${bookname.toString()}"
+            val body = "I want To Buy ${bookname.toString()} Book of Subject ${sub.toString()} at Price ${price.toString()}₹"
+            val mailTo = "mailto:" + to +
+                    "?&subject=" + Uri.encode(subject) +
+                    "&body=" + Uri.encode(body)
+            val emailIntent = Intent(Intent.ACTION_VIEW)
+            emailIntent.data = Uri.parse(mailTo)
+            startActivity(emailIntent)
+        })
+
+        btnsms.setOnClickListener(View.OnClickListener { v: View? ->
+            val uri = Uri.parse("smsto:+91${number.text}")
+            val intent = Intent(Intent.ACTION_SENDTO, uri)
+            intent.putExtra("sms_body", "I want To Buy ${bookname.toString()} Book of Subject ${sub.toString()} at Price ${price.toString()}₹")
+            startActivity(intent)
+        })
+
         val userREf=database.getReference("user")
         userREf.child(uid!!).addListenerForSingleValueEvent(object :ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
@@ -80,7 +103,9 @@ class book_details : AppCompatActivity() {
                 if(uvalue!=null){
                     Uploadby.setText(uvalue.username)
                     email.setText(uvalue.email)
+
                     number.setText(uvalue.mobile_number)
+
                 }
             }
 
@@ -89,34 +114,6 @@ class book_details : AppCompatActivity() {
             }
 
         })
-        fun sendEmail(recipient: String, subject: String, message: String) {
-            /*ACTION_SEND action to launch an email client installed on your Android device.*/
-            val mIntent = Intent(Intent.ACTION_SEND)
-            /*To send an email you need to specify mailto: as URI using setData() method
-            and data type will be to text/plain using setType() method*/
-            mIntent.data = Uri.parse("mailto:")
-            mIntent.type = "text/plain"
-            // put recipient email in intent
-            /* recipient is put as array because you may wanna send email to multiple emails
-               so enter comma(,) separated emails, it will be stored in array*/
-            mIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(recipient))
-            //put the Subject in the intent
-            mIntent.putExtra(Intent.EXTRA_SUBJECT, subject)
-            //put the message in the intent
-            mIntent.putExtra(Intent.EXTRA_TEXT, message)
 
-
-            try {
-                //start email intent
-                startActivity(Intent.createChooser(mIntent, "Choose Email Client..."))
-            }
-            catch (e: Exception){
-                //if any thing goes wrong for example no email client application or any exception
-                //get and show exception message
-                Toast.makeText(this, e.message, Toast.LENGTH_LONG).show()
-            }
-
-
-    }
     }
 }
--- app/src/main/java/com/example/charusatbookstore/cmpicadepartment.kt
+++ app/src/main/java/com/example/charusatbookstore/cmpicadepartment.kt
@@ -1,9 +1,14 @@
 package com.example.charusatbookstore
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
 
 class cmpicadepartment : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +23,42 @@ class cmpicadepartment : AppCompatActivity() {
         ib1.setOnClickListener {
             val Intent= Intent(this,sem::class.java)
             startActivity(Intent)
+        }
+    }
+    //otion menue
+    override fun onCreateOptionsMenu (menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu,menu)
+        return true
+    }
+    //option menue commands and next activity
+    override fun onOptionsItemSelected(item: MenuItem):Boolean{
+        return when(item.itemId){
+            R.id.logout->{
+                FirebaseAuth.getInstance().signOut()
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("LOGOUT")
+                builder.setMessage("Do You want to logout from Applicatiom?")
+                builder.setPositiveButton("Yes",{ dialogInterface : DialogInterface, i : Int ->
+                    finish()
+                    startActivity(Intent(this, login::class.java))})
+                builder.setNegativeButton("No",{ dialogInterface : DialogInterface, i: Int ->})
+                builder.show()
+                true
+            }
+            R.id.edit->{
+                startActivity(Intent(this, Edit_profile::class.java))
+                true
+            }
+            R.id.ubook->{
+                startActivity(Intent(this,upload::class.java))
+                true
+            }
+            R.id.mybooks->{
+                startActivity(Intent(this,User_book::class.java))
+                true
+            }
+            else ->super.onOptionsItemSelected(item)
+
         }
     }
 }
